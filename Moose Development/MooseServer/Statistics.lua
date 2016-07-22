0c0aee3b999285e759d29a15b25a33559cb4ec87
HubJack <hijacknor@gmail.com>
2016-07-22 12:17:24 +0200

    Rebuild with own moose folder for logfiles
    Changed everything to accomplish logging of player, mission and round data to each file. Mission will change but register in the same file as long as the server is not restarted. The player information file will be the same and register all connecting players in to the same file as long as the server is not restarted. The round file is recording player times for each mission round and will change to a new filename for each mission.

--- Moose Development/MooseServer/Statistics.lua
+++ Moose Development/MooseServer/Statistics.lua
@@ -1,41 +1,73 @@
-local MooseCallBacks = {}
+do
 
-function MooseCallBacks.onPlayerTryConnect(ipaddr, name, ucid, playerID)
-    print('onPlayerTryConnect(%s, %s, %s, %d)', ipaddr, name, ucid, playerID)
-    -- if you want to gently intercept the call, allowing other user scripts to get it,
-    -- you better return nothing here
+local MooseCallBacks = {}
+loadID = tostring(os.date("%Y%m%d%H%M%S"))
+loadDate = tostring(os.date("%Y-%m-%d"))
+loadTime = tostring(os.date("%H:%M:%S"))
+roundID = loadID
+roundCount = 0
+mlog = io.open(lfs.writedir() .. "moose/missioninfo_" .. loadID .. ".log", "w")
+mlog:write('roundID, simTime, missiontime, cur_mission\n')
+plog = io.open(lfs.writedir() .. "moose/playerinfo_" .. loadID .. ".log", "w")
+plog:write( "PlayerUCID, PlayerName, PlayerIP\n")
+
+function MooseCallBacks.onPlayerTryConnect(addr, name, ucid, playerID)
     return true -- allow the player to connect
 end
 
+function MooseCallBacks.onPlayerStart(id)
+  local simTime = DCS.getModelTime()
+  local uniqueIndex = tostring(os.date("%Y%m%d%H%M%S")) .. string.format( "%g", simTime)
+  local PlayerName = net.get_player_info( id, "name" )
+  local playerDate = tostring(os.date("%Y-%m-%d"))
+  local playerTime = tostring(os.date("%H:%M:%S"))
+  rlog:write( string.format( '"%s", "%s", "%s", "%s", "%8f", "%s", "%s"\n', uniqueIndex, roundID, playerDate, playerTime, simTime, "player_connect", PlayerName ) )
+end
+
+function MooseCallBacks.onPlayerConnect(id, name)
+    local PlayerName = net.get_player_info( id, 'name' )
+    local PlayerIP = net.get_player_info( id, 'ipaddr' )
+    local PlayerUCID = net.get_player_info( id, 'ucid' )
+    plog:write(string.format( '"%s", "%s", "%s"\n', PlayerUCID, PlayerName, PlayerIP))
+  return
+end
+
 function MooseCallBacks.onSimulationStart()
-    print('Current mission is '..DCS.getMissionName())
+
+end
+
+function MooseCallBacks.onMissionLoadBegin()
+  roundID = tostring(os.date("%Y%m%d%H%M%S"))
+end
+
+function MooseCallBacks.onMissionLoadEnd()
+  local cur_mission = tostring(DCS.getMissionName())
+  local missiontime = tostring(os.date("%Y-%m-%d %H:%M:%S"))
+  local simTime = DCS.getModelTime()
+  mlog:write( string.format( '"%s", "%8f", "%s", "%s"\n', roundID, simTime, missiontime, cur_mission))
+  if roundCount == 0 then
+    rlog = io.open(lfs.writedir() .. "moose/roundinfo_" .. loadID .. ".log", "w")
+    rlog:write( "uniqueIndex, roundID, Date, Time, simTime, eventName, PlayerName\n")
+    roundCount = 1  
+  else
+    rlog:close()
+    rlog = nil
+    rlog = io.open(lfs.writedir() .. "moose/roundinfo_" .. roundID .. ".log", "w")
+    rlog:write( "uniqueIndex, roundID, Date, Time, simTime, eventName, PlayerName\n")
+  end
 end
 
 function MooseCallBacks.onGameEvent( eventName, arg1, arg2, arg3, arg4 ) 
---"friendly_fire", playerID, weaponName, victimPlayerID
---"mission_end", winner, msg
---"kill", killerPlayerID, killerUnitType, killerSide, victimPlayerID, victimUnitType, victimSide, weaponName
---"self_kill", playerID
---"change_slot", playerID, slotID, prevSide
---"connect", id, name
---"disconnect", ID_, name, playerSide
---"crash", playerID, unit_missionID
---"eject", playerID, unit_missionID
---"takeoff", playerID, unit_missionID, airdromeName
---"landing", playerID, unit_missionID, airdromeName
---"pilot_death", playerID, unit_missionID
-	local Time = DCS.getModelTime()
-	if eventName == "self_kill" or eventName == "change_slot" or eventName == "crash" or eventName == "eject" or eventName == "takeoff" or eventName == "landing" or eventName == "pilot_death" then
-		local PlayerName = net.get_player_info( arg1, "name" )
-		log.write( 'MooseFT', log.INFO, string.format( '"%8f", "%s", "%s"\n', Time, eventName, PlayerName ) )
-	end
+  if eventName == "self_kill" or eventName == "change_slot" or eventName == "crash" or eventName == "eject" or eventName == "takeoff" or eventName == "landing" or eventName == "pilot_death" then
+    local simTime = DCS.getModelTime()
+    local realTime = DCS.getRealTime()
+    local uniqueIndex = tostring(os.date("%Y%m%d%H%M%S")) .. string.format( "%g", simTime)
+  	local playerDate = tostring(os.date("%Y-%m-%d"))
+  	local playerTime = tostring(os.date("%H:%M:%S"))
+    local PlayerName = net.get_player_info( arg1, "name" )
+    rlog:write( string.format( '"%s", "%s", "%s", "%s", "%8f", "%s", "%s"\n', uniqueIndex, roundID, playerDate, playerTime, simTime, eventName, PlayerName ) )
+  end
 end
 
-function MooseCallBacks.onSimulationPause()
-  log.write( 'MooseFT', log.INFO, "Simulation paused.\n" )
+DCS.setUserCallbacks( MooseCallBacks )
 end
-
--- Add date and time as file ID to identify logstart. Dues not need to be user understandable, just unique.
-local logtime = tostring(os.date("%Y%m%d%I%M%S"))
-log.set_output( 'Moose-FlightTimes_' .. logtime, 'MooseFT', log.TRACE + log.ALL, log.MESSAGE + log.TIME + log.LEVEL )
-DCS.setUserCallbacks( MooseCallBacks )  -- here we set our callbacks
